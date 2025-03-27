179ffd543e5c29b1ed048004838149b970198c45
Sardlimad <genesis.david.08@gmail.com>
2025-03-27 00:22:57 -0400

    Modifications for production deployment

--- src/config/dbDesarrollo.js
+++ src/config/dbDesarrollo.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT,
+    logging: false,
   }
-});
+);
 
 sequelize.authenticate()
   .then(() => console.log('Conectado a PostgreSQL'))
